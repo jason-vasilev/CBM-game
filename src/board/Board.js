b1c8432769b9d2f5619e951560a453c854ffdea0
Yasen Vasilev <yvasilev@vertic.com>
2021-11-19 11:16:10 +0100

    Moved logic to Bord componenet

--- src/board/Board.js
+++ src/board/Board.js
@@ -8,11 +8,27 @@ function Board() {
 	const isHappyCardPosition = Math.floor(Math.random() * cards.length);
 	cards[isHappyCardPosition] = true;
 
+	const onCardClick = (e) => {
+		const { target } = e;
+
+		if (!target) {
+			return;
+		}
+
+		const happyCard = target.classList.contains('card--happy');
+
+		if (!happyCard) {
+			target.classList.add('card--faded');
+		} else {
+			console.log('YES');
+		}
+	};
+
 	return (
 		<section className="board">
 			{cards.map((card, index) => {
 				// eslint-disable-next-line react/no-array-index-key
-				return <Card key={index} isHappy={card} />;
+				return <Card key={index} isHappy={card} onCardClick={onCardClick} />;
 			})}
 		</section>
 	);
--- src/card/Card.js
+++ src/card/Card.js
@@ -3,25 +3,9 @@ import PropTypes from 'prop-types';
 import './index.css';
 
 function Card(props) {
-	const handleClick = (e) => {
-		const { target } = e;
-
-		if (!target) {
-			return;
-		}
-
-		const happyCard = target.classList.contains('card--happy');
-
-		if (!happyCard) {
-			target.classList.add('card--faded');
-		} else {
-			console.log('YES');
-		}
-	};
-
 	return (
 		// eslint-disable-next-line react/destructuring-assignment
-		<button onClick={handleClick} type="button" className={`card${props.isHappy ? ' card--happy' : ''}`}>
+		<button onClick={props.onCardClick} type="button" className={`card${props.isHappy ? ' card--happy' : ''}`}>
 			<img src="https://google.com/" alt="" />
 		</button>
 	);
@@ -33,6 +17,7 @@ Card.defaultProps = {
 
 Card.propTypes = {
 	isHappy: PropTypes.bool,
+	onCardClick: PropTypes.func.isRequired,
 };
 
 export default Card;
