d044de16d22c5ca80a3f62e0d4b6f2bf1e56aaba
Yasen Vasilev <yvasilev@vertic.com>
2021-11-19 14:44:08 +0100

    Initial rounds setup

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Card from '../card/Card';
 
 function Board() {
 	const cards = [null, null, null, null, null, null, null, null, null];
-
 	const isHappyCardPosition = Math.floor(Math.random() * cards.length);
 	cards[isHappyCardPosition] = true;
+
+	// eslint-disable-next-line no-unused-vars
+	const [rounds, roundsCounter] = useState(0);
 
 	const onCardClick = (e) => {
 		const { target } = e;
 
-		if (!target) {
-			return;
-		}
+		if (!target) { return; }
 
 		const happyCard = target.classList.contains('card--happy');
 
 		if (!happyCard) {
 			target.classList.add('card--faded');
-		} else {
-			console.log('YES');
+		} else if (rounds < 5) {
+			const targetSiblings = Array.from(target.parentElement.children);
+
+			targetSiblings.forEach((sibling) => {
+				if (sibling.classList.contains('card--faded')) {
+					sibling.classList.remove('card--faded');
+				}
+			});
+
+			roundsCounter(rounds + 1);
 		}
 	};
 
 	return (
-		<section className="board">
-			{cards.map((card, index) => {
-				// eslint-disable-next-line react/no-array-index-key
-				return <Card key={index} isHappy={card} onCardClick={onCardClick} />;
-			})}
-		</section>
+		<>
+			<h2>{`${rounds} of 5`}</h2>
+
+			<section className="board">
+				{rounds < 5 && (
+					<>
+						{cards.map((card, index) => {
+							// eslint-disable-next-line react/no-array-index-key
+							return <Card key={index} isHappy={card} onCardClick={onCardClick} />;
+						})}
+					</>
+				) }
+			</section>
+		</>
 	);
 }
 
